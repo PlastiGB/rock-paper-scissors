465fb45b77ccc581fd9cdfd53fd7893627fd265b
Samet Kandemir <51782139+PlastiGB@users.noreply.github.com>
2021-11-28 16:59:38 +0300

    Add index.html, script.js, style.css, /images

---
+++ script.js
@@ -0,0 +1,108 @@
+let _cpuRPS;
+let _playerRPS;
+
+let _point;
+let _playerScore = 0;
+let _cpuScore = 0;
+let _result = 0;
+
+let _roundCounter = 0;
+
+let RPS = {
+    "Rock": 1,
+    "Paper": 2,
+    "Scissors": 3 
+};
+
+let WLT = {
+    1: "Win",
+    2: "Lose",
+    3: "Tie"
+}
+
+function Game(player)
+{
+    _roundCounter += parseInt(1);
+
+    if(_roundCounter <= 5)
+    {
+        _playerRPS = player;
+        cpuPlay();
+
+        _playerRPS = _playerRPS[0].toUpperCase() + _playerRPS.slice(1).toLowerCase();
+        _cpuRPS = _cpuRPS[0].toUpperCase() + _cpuRPS.slice(1).toLowerCase();
+
+        Round(_playerRPS, _cpuRPS)
+
+        switch(_point)
+        {
+            case 1:
+                _playerScore += 1;
+
+                console.log(`You Win! ${_playerRPS} beats ${_cpuRPS}`);
+                break;
+        case 2:
+                _cpuScore += 1;
+                console.log(`You Lose! ${_cpuRPS} beats ${_playerRPS}`);
+                break;
+        }
+    }
+    else
+    {
+        _result = _playerScore > _cpuScore ? "You Win!" : "You Lose!";
+        console.log(_result);
+    }
+}
+
+function Round(playerRPS, cpuRPS)
+{
+    switch(RPS[playerRPS])
+    {
+        case 1:
+            _point = RPS[cpuRPS] == 1 ? 3 :
+                    RPS[cpuRPS] == 2 ? 2 : 1;
+
+            console.log(WLT[_point]);
+            break;
+        case 2:
+            _point = RPS[cpuRPS] == 1 ? 1 :
+                    RPS[cpuRPS] == 2 ? 3 : 2;
+
+            console.log(WLT[_point]);
+            break;
+        case 3:
+            _point = RPS[cpuRPS] == 1 ? 2 :
+                    RPS[cpuRPS] == 2 ? 1 : 3;
+
+            console.log(WLT[_point]);
+            break;
+    }
+}
+
+function cpuPlay()
+{
+    _cpuRPS = cpuRandomGen(_cpuRPS);
+}
+
+function cpuRandomGen(cpuRPS)
+{
+    cpuRPS = Math.floor((Math.random() * 3) + 1);
+
+    switch(cpuRPS)
+    {
+        case 1:
+            document.getElementById("cpu").src = "images/Rock.png";
+            break;
+        case 2:
+            document.getElementById("cpu").src = "images/Paper.png";
+            break;
+        case 3:
+            document.getElementById("cpu").src = "images/Scissors.png";
+            break;
+    }
+
+    cpuRPS = cpuRPS == 1 ? "Rock" : 
+            cpuRPS == 2 ? "Paper" : "Scissors";
+
+    return cpuRPS;
+}
